54b898ef0b5cd2fb22ec6be47426f159ad27c420
AlexPit <alexpit63@gmail.com>
2019-05-22 16:37:44 +0300

    Initial commit

---
+++ src/main/java/ru/sber/alex/minibank/MinibankApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.alex.minibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibankApplication.class, args);
+	}
+
+}
