593601dde6f001773eb6bf7959c531e4f88b77ef
AlexPit <alexpit63@gmail.com>
2019-05-30 16:46:25 +0300

    прикрутил bootstrap с навигационной панелью, прикрутил красивый стиль; добавил формы и каркасные методы обработки страниц управления денежными средствами; на данный момент с фронтом закончено, нужно описывать логику. К фронту вернусь, когда буду выводить транзакции операций.

--- src/main/java/ru/sber/alex/minibank/layers/controllers/ServiceController.java
+++ src/main/java/ru/sber/alex/minibank/layers/controllers/ServiceController.java
@@ -1,23 +1,12 @@
 package ru.sber.alex.minibank.layers.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.sber.alex.minibank.dto.ClientDto;
-import ru.sber.alex.minibank.layers.logic.BusinessLogic;
-
-import java.util.Map;
-
+import ru.sber.alex.minibank.dto.TransactionDto;
 @Controller
 public class ServiceController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required = false, defaultValue = "World") String name, Map model){
-        model.put("name", name);
-        return "greeting";
-    }
-    //home page mapping
     @GetMapping("/")
     public String index() {
         return "index";
@@ -32,15 +21,44 @@ public class ServiceController {
     public String login(){
         return "login";
     }
-/*
-    @GetMapping("/logout")
-    public String logout(){
-        return "index";
-    }
-*/
+
     @GetMapping("/personaloffice")
     public String persOffice(){
         return "personaloffice";
     }
 
+    @GetMapping("/push")
+    public String pushMoneyGet(Model model){
+        model.addAttribute("transaction", new TransactionDto());
+        return "pushmoney";
+    }
+
+    @PostMapping("/push")
+    public String pushMoneyPost(@ModelAttribute TransactionDto transaction, Model model){
+
+        return "pushmoney";
+    }
+
+    @GetMapping("/pull")
+    public String pullMoneyGet(Model model){
+        model.addAttribute("transaction", new TransactionDto());
+        return "pullmoney";
+    }
+
+    @PostMapping("/pull")
+    public String pullMoneyPost(@ModelAttribute TransactionDto transaction, Model model){
+
+        return "pullmoney";
+    }
+
+    @GetMapping("/transaction")
+    public String transactionGet(Model model){
+        model.addAttribute("transaction", new TransactionDto());
+        return "transaction";
+    }
+
+    @PostMapping("/transaction")
+    public String transactionPost(@ModelAttribute TransactionDto transaction, Model model){
+        return "transaction";
+    }
 }
